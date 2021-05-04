6dbf9d7ce5547960b84fd38a3b5461c28cf36ca2
tae080 <thj994020@gmail.com>
2021-05-04 21:25:49 +0700

    working on input js

--- input.js
+++ input.js
@@ -1,36 +1,57 @@
-const form = document.getElementById('form');
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-
-function setErrorFor(input, message) {
-const formControl = input.parentElement;
-const small = formControl.querySelector('small');
-formControl.className = 'form error';
-small.innerText = message;
-}
-
-function setSuccessFor(input) {
-const formControl = input.parentElement;
-formControl.className = 'form success';
-}
-
-function validateName(){
+btn = document.querySelector(#submit);
+btn.addEventListener("click",function(ev)){
+  const form = document.getElementById('form').value;
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
 
   var nameRegExp = /^[a-zA-Z]{3,}$/;
   var emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-  if (name===nameRegExp){
-    setSuccessFor(name)
+  if (nameRegExp.test(name)){
+    // setSuccessFor(name)
+    alert("valid");
   }
   else {
-    setErrorFor(name)
-  }
-
-  if (email===emailRegExp){
-    setSuccessFor(email);
-  }
-  else {
-    setErrorFor(email);
+    // setErrorFor(name)
+    alert("invaid");
   }
 }
+//
+// function setErrorFor(input, message) {
+// const formControl = input.parentElement;
+// const small = formControl.querySelector('small');
+// formControl.className = 'form error';
+// small.innerText = message;
+// }
+//
+// function setSuccessFor(input) {
+// const formControl = input.parentElement;
+// formControl.className = 'form success';
+// }
+//
+// function validate(){
+//   const form = document.getElementById('form').value;
+//   const name = document.getElementById('name').value;
+//   const email = document.getElementById('email').value;
+//   const phone = document.getElementById('phone').value;
+//
+//   var nameRegExp = /^[a-zA-Z]{3,}$/;
+//   var emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//
+//   if (nameRegExp.test(name)){
+//     // setSuccessFor(name)
+//     alert("valid");
+//   }
+//   else {
+//     // setErrorFor(name)
+//     alert("invaid");
+//   }
+//
+//   if (email===emailRegExp){
+//     setSuccessFor(email);
+//   }
+//   else {
+//     setErrorFor(email);
+//   }
+// }
