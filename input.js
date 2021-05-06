13ca69a0b2ce83720ceda092cfb992c1276f110f
taehyeon09 <81557238+taehyeon09@users.noreply.github.com>
2021-05-07 00:03:09 +0700

    revised format

--- input.js
+++ input.js
@@ -1,46 +1,41 @@
-//
-  var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
-  var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
-  var phoneRegExp = new RegExp(/^\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d?([ .-]?)\d?([ .-]?){9,11}$/);
-  // var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$);
-  // var zipRegExp = new RegExp(/^\d{4,6}$);
+var nameRegExp = /^[a-zA-Z]{3,}$/;
+var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
+var phoneRegExp = new RegExp(/^\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d?([ .-]?)\d?([ .-]?){9,11}$/)
+var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/);
 
+var atleast3charRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
 
-  // const form = document.getElementById('form').value;
 
 //for name validation
 function nameValidation(){
   var name = document.getElementById('name').value;
-  // console.log(name) //Find an element by element id
-  var nameRegExp = /^[a-zA-Z]{3,}$/;
+  var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
 
   if (nameRegExp.test(name)){
-    // console.log("Correct");
     return true;
   } else{
-    // console.log("Not correct");
-    // document.getElementById("name").focus;
     return false;
   }
 }
 
-
 // Step 1: Get the element you want to listen
-var nameInput = document.getElementById('name');
+// var nameInput = document.getElementById('name').value;
 // console.log(nameInput.value);
-nameInput.addEventListener('input', nameValidation);
+// nameInput.addEventListener('input', nameValidation);
 //
+
 //for email validation
 function emailValidation(){
   const email = document.getElementById('email');
   var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
 
   if (emailRegExp.test(email)) {
+    console.log("no")
     return true;
   }
 
   else {
-    // document.myform.email.focus;
+    console.log("yes")
     return false;
   }
 }
@@ -55,26 +50,21 @@ function phoneValidation(){
   return false;
 }
 
-//for radio validation
+// for radio validation
 function radioValidation(){
 var valid = false;
-var x = document.myform.perf_contact;
 
 for(var i=0;i<x.length;i++){
   if(x[i].checked){
     valid = true; //valid when only one option is selected
+    console.log("yesss")
     break;
   }
   else{
-    // alert("Please select a contact option");
     return false;
   }
  }
 };
-
-// function passwordValidation(){
-//   var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$);
-// }
 
 const textarea = document.getElementById('Message');
 const remainingCharsText = document.getElementById('remaining-char');
@@ -95,9 +85,68 @@ textarea.addEventListener('input',() => {
         remainingCharsText.textContent = `Deleting ${x - 500} letters is needed.`;
     }
 });
+
+//for password validation
+function passwordValidation(){
+  var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/);
+  var password = document.getElementById("password");
+
+  if (passwordRegExp.test(password)){
+    return true;
+  }
+  return false;
+}
+
+function conPassword(){
+  var password = document.getElementById("password");
+  var confirmPassword = document.getElementById("confirmpassword");
+
+  if (password != confirmpassword){
+    return false;
+  }
+  return true;
+}
+
+//for 3 char
+function atleast3char(){
+  var fname = document.getElementById("fname").value;
+  var lname = document.getElementById("lname").value;
+  var address = document.getElementById("address").value;
+  var city = document.getElementById("city").value;
+
+  var atleast3charRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
+
+  if (atleast3charRegExp.test(fname)){
+    return true;
+  }
+  if (atleast3charRegExp.test(lname)){
+    return true;
+  }
+  if (atleast3charRegExp.test(address)){
+    return true;
+  }
+  if (atleast3charRegExp.test(city)){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+//for zipcode
+function zipcodevalidation(){
+  var zipcode = document.getElementById("zipcode");
+  var zipRegExp = new RegExp(/^\d{4,6}$/);
+
+  if(zipRegExp.test(zipcode)){
+    return true;
+  }
+  return false;
+}
+
 // name.addEventListener("input",function(event)
 //Final
-var submit = document.getElementById("submit");
+var submit = document.getElementById("contact-submit");
 // console.log(submit);
 // var contactForm = document.getElementById("contact-form");
 
@@ -107,7 +156,6 @@ submit.addEventListener("click", function(e){
   console.log(emailValidation());
   console.log(radioValidation());
   if (
-
     nameValidation()
     && emailValidation()
     && phoneValidation()
