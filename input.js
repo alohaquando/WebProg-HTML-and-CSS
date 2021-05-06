3eb507a8dd6c45a85720571111e1cec728db61ca
taehyeon09 <81557238+taehyeon09@users.noreply.github.com>
2021-05-06 16:00:22 +0700

    add textbox validation

--- input.js
+++ input.js
@@ -2,25 +2,33 @@
   var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
   var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
   var phoneRegExp = new RegExp(/^\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d?([ .-]?)\d?([ .-]?){9,11}$/)
-  var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$);
-  var zipRegExp = new RegExp(/^\d{4,6}$);
+  // var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$);
+  // var zipRegExp = new RegExp(/^\d{4,6}$);
 
 
   // const form = document.getElementById('form').value;
 
 //for name validation
 function nameValidation(){
-  var name = document.getElementById('name'); //Find an element by element id
-  var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
+  var name = document.getElementById('name').value;
+  // console.log(name) //Find an element by element id
+  var nameRegExp = /^[a-zA-Z]{3,}$/;
 
   if (nameRegExp.test(name)){
+    // console.log("Correct");
     return true;
-  }
-  else{
-    document.myform.name.focus;
+  } else{
+    // console.log("Not correct");
+    // document.getElementById("name").focus;
     return false;
   }
 }
+
+
+// Step 1: Get the element you want to listen
+var nameInput = document.getElementById('name');
+// console.log(nameInput.value);
+nameInput.addEventListener('input', nameValidation);
 //
 //for email validation
 function emailValidation(){
@@ -30,8 +38,9 @@ function emailValidation(){
   if (emailRegExp.test(email)) {
     return true;
   }
+
   else {
-    document.myform.email.focus;
+    // document.myform.email.focus;
     return false;
   }
 }
@@ -63,15 +72,42 @@ for(var i=0;i<x.length;i++){
  }
 };
 
-function passwordValidation(){
-  var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$);
-}
+// function passwordValidation(){
+//   var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$);
+// }
 
+const textarea = document.getElementById('Message');
+const remainingCharsText = document.getElementById('remaining-char');
+const Max_Chars = 500;
+const Min_Chars = 50;
+
+textarea.addEventListener('input',() => {
+    const remaining500 = Max_Chars - textarea.value.length;
+    const remaining50 = Min_Chars - textarea.value.length;
+    let x = textarea.value.length;
+    if (x < 50) {
+        remainingCharsText.textContent = ` ${50 - x} more lettrs are needed.`; 
+    }
+    else if (50 < x && x < 500) {
+        remainingCharsText.textContent = `You can type ${500 - x} more letters.`; 
+    }
+    else if (x > 500) {
+        remainingCharsText.textContent = `Deleting ${x - 500} letters is needed.`; 
+    }
+});
 // name.addEventListener("input",function(event)
 //Final
-submit = document.querySelector("#submit");
-submit.addEventListener("click",function(event){
+var submit = document.getElementById("submit");
+// console.log(submit);
+// var contactForm = document.getElementById("contact-form");
+
+submit.addEventListener("click", function(e){
+  e.preventDefault()
+  console.log(nameValidation());
+  console.log(emailValidation());
+  console.log(radioValidation());
   if (
+
     nameValidation()
     && emailValidation()
     && phoneValidation()
@@ -84,15 +120,30 @@ submit.addEventListener("click",function(event){
   }
 });
 
-  const form = document.getElementById('form');
-  const name = document.getElementById('name');
-  const email = document.getElementById('email');
+// document.getElementById('test-button').addEventListener("click",function(event){
+//   console.log(nameValidation());
+//   if (
+//     nameValidation()
+//     // && emailValidation()
+//     // && phoneValidation()
+//     // && radioValidation()
+//   ) {
+//     alert("valid");
+//   }
+//   else {
+//     alert("invalid");
+//   }
+// })
 
-  form.addEventListener('submit',(e) => {
-    e.preventDefault()
-  });
+//   const form = document.getElementById('form');
+//   const name = document.getElementById('name');
+//   const email = document.getElementById('email');
+
+//   form.addEventListener('submit',(e) => {
+//     e.preventDefault()
+//   });
 
 //additional fields for store owner
-function owner(){
-  
-}
+// function owner(){
+//
+// }
