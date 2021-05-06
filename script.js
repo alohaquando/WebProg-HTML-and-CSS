90305adcaa2ed14d0dfdb18ea7e6bbbc24a94fdc
taehyeon09 <81557238+taehyeon09@users.noreply.github.com>
2021-05-06 15:55:41 +0700

    Revert "Merge branch 'main' into 4-contact-form-validation"
    This reverts commit c5670677b6216f36266519ca675660e6f742b682, reversing
    changes made to 8943ebe2e530bcc5bc7401550235de9016f25bdb.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Header and footer
 $(function () {
   $("#nav_header").load("./nav_header.html");
 });
@@ -14,8 +13,6 @@ $(function () {
 $(function () {
   $("#store_header").load("./store_header.html");
 });
-
-// Shopping cart add and remove
 function wcqib_refresh_quantity_increments() {
   jQuery(
     "div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)"
@@ -32,7 +29,6 @@ function wcqib_refresh_quantity_increments() {
         .after('<input type="button" value="+" class="plus" />');
   });
 }
-
 String.prototype.getDecimals ||
   (String.prototype.getDecimals = function () {
     var a = this,
@@ -65,20 +61,3 @@ String.prototype.getDecimals ||
         : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())),
       a.trigger("change");
   });
-
-// About Us Modal
-var open_modal = document.querySelectorAll(".open_modal");
-for (i = 0; i < open_modal.length; i++) {
-  open_modal[i].onclick = function () {
-    var modal_name = this.getAttribute("data-modal");
-    document.getElementById(modal_name).style.display = "flex";
-  };
-}
-
-const modal = document.querySelectorAll(".modal");
-
-function close_modal() {
-  for (i = 0; i < modal.length; i++) {
-    modal[i].style.display = "none";
-  }
-}
