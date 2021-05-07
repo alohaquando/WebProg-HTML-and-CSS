5ae51dd97e5017bc186b92ec0e9010560639b120
taehyeon09 <81557238+taehyeon09@users.noreply.github.com>
2021-05-07 15:03:08 +0700

    update

--- input.js
+++ input.js
@@ -1,37 +1,202 @@
-// var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
-// var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
-// var phoneRegExp = new RegExp(/^\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d?([ .-]?)\d?([ .-]?){9,11}$/)
-// var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/);
-// var atleast3charRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
+// // var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
+// // var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
+// // var phoneRegExp = new RegExp(/^\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d?([ .-]?)\d?([ .-]?){9,11}$/)
+// // var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/);
+// // var atleast3charRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
+//
+// //for name validation
+// function nameValidation() {
+//   var name = document.getElementById('name').value;
+//   var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
+//
+//   if (nameRegExp.test(name)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+//
+// //for email validation
+// function emailValidation() {
+//   const email = document.getElementById('email').value;
+//   var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
+//
+//   if (emailRegExp.test(email)) {
+//     console.log("noo")
+//     return true;
+//   } else {
+//     console.log("yess")
+//     return false;
+//   }
+// }
+//
+// //for phone Validation
+// function phoneValidation() {
+//   const phone = document.getElementById('phone');
+//   var phoneRegExp = new RegExp(/^\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d?([ .-]?)\d?([ .-]?){9,11}$/)
+//
+//   if (phoneRegExp.test(phone)) {
+//     return true;
+//   }
+//   return false;
+// }
+//
+// //for textarea Validation
+// var textarea = document.getElementById('Message');
+// var remainingCharsText = document.getElementById('remaining-char');
+// var Max_Chars = 500;
+// var Min_Chars = 50;
+//
+// textarea.addEventListener('input', textareaValidation)
+//
+// function textareaValidation() {
+//   var remaining500 = Max_Chars - textarea.value.length;
+//   var remaining50 = Min_Chars - textarea.value.length;
+//   let x = textarea.value.length;
+//   if (x < 50) {
+//     remainingCharsText.textContent = ` ${50 - x} more lettrs are needed.`;
+//   } else if (50 < x && x < 500) {
+//     remainingCharsText.textContent = `You can type ${500 - x} more letters.`;
+//   } else if (x > 500) {
+//     remainingCharsText.textContent = `Deleting ${x - 500} letters is needed.`;
+//   }
+// }
+//
+// //contact page submit
+// document.getElementById('contact-submit').addEventListener("click", submit(e))
+// function submit(e) {
+//   if (
+//     nameValidation() &&
+//     emailValidation() &&
+//     phoneValidation() &&
+//     textareaValidation()
+//   ) {
+//     alert("valid");
+//   } else {
+//     alert("invalid");
+//   }
+// }
+//
+// //for password validation
+// function passwordValidation() {
+//   var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/);
+//   var password = document.getElementById("password");
+//
+//   if (passwordRegExp.test(password)) {
+//     return true;
+//   }
+//   return false;
+// }
+//
+// function conPassword() {
+//   var password = document.getElementById("password");
+//   var confirmPassword = document.getElementById("confirmpassword");
+//
+//   if (password != confirmpassword) {
+//     return false;
+//   }
+//   return true;
+// }
+//
+// //for 3 char
+// function atleast3char() {
+//   var fname = document.getElementById("fname").value;
+//   var lname = document.getElementById("lname").value;
+//   var address = document.getElementById("address").value;
+//   var city = document.getElementById("city").value;
+//
+//   var atleast3charRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
+//
+//   if (atleast3charRegExp.test(fname)) {
+//     return true;
+//   }
+//   if (atleast3charRegExp.test(lname)) {
+//     return true;
+//   }
+//   if (atleast3charRegExp.test(address)) {
+//     return true;
+//   }
+//   if (atleast3charRegExp.test(city)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+//
+// //for zipcode
+// function zipcodevalidation() {
+//   var zipcode = document.getElementById("zipcode").value;
+//   var zipRegExp = new RegExp(/^\d{4,6}$/);
+//
+//   if (zipRegExp.test(zipcode)) {
+//     return true;
+//   }
+//   return false;
+// }
+//
+// //for profile image
+// var imageAvatar = document.querySelector('#oval-img');
+//
+// function check_img_input(input) {
+//   if (input.files.length == 0) {
+//     return 0;
+//   } else {
+//     return 1;
+//   }
+// }
+//
+// function change_img(input) {
+//   if (check_img_input(input)) {
+//     const file = document.querySelector('input[type=file]').files[0];
+//     const reader = new FileReader();
+//     reader.addEventListener("load", function() {
+//       // convert image file to base64 string
+//       imageAvatar.src = reader.result;
+//     }, false);
+//
+//     if (file) {
+//       reader.readAsDataURL(file);
+//     }
+//   }
+// }
+//
+// //for exposing additional fields
+// var shopper = document.getElementById("store_owener");
+//
+// shopper.click(function radio() {
+//       (".reveal-if-active").show();
+//     }
 
 //for name validation
-function nameValidation() {
+function nameValidation(){
   var name = document.getElementById('name').value;
   var nameRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
 
-  if (nameRegExp.test(name)) {
+  if (nameRegExp.test(name)){
     return true;
-  } else {
+  } else{
     return false;
   }
 }
 
 //for email validation
-function emailValidation() {
+function emailValidation(){
   const email = document.getElementById('email').value;
   var emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/);
 
   if (emailRegExp.test(email)) {
     console.log("no")
     return true;
-  } else {
+  }
+
+  else {
     console.log("yes")
     return false;
   }
 }
 
 //for phone Validation
-function phoneValidation() {
+function phoneValidation(){
   const phone = document.getElementById('phone');
   var phoneRegExp = new RegExp(/^\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d([ .-]?)\d?([ .-]?)\d?([ .-]?){9,11}$/)
 
@@ -41,66 +206,92 @@ function phoneValidation() {
   return false;
 }
 
-//for textarea Validation
+// for radio validation
+function radioValidation(){
+var valid = false;
+var x = document.getElementById("radio");
+
+for(var i=0;i<x.length;i++){
+  if(x[i].checked){
+    console.log("yesss")
+    valid = true; //valid when only one option is selected
+    break;
+  }
+  else{
+    console.log("no")
+    return false;
+  }
+ }
+};
+
 var textarea = document.getElementById('Message');
 var remainingCharsText = document.getElementById('remaining-char');
 var Max_Chars = 500;
 var Min_Chars = 50;
 
-textarea.addEventListener('input', textareaValidation)
+textarea.addEventListener('input',() => {
+    var remaining500 = Max_Chars - textarea.value.length;
+    var remaining50 = Min_Chars - textarea.value.length;
+    let x = textarea.value.length;
+    if (x < 50) {
+        remainingCharsText.textContent = ` ${50 - x} more lettrs are needed.`;
+    }
+    else if (50 < x && x < 500) {
+        remainingCharsText.textContent = `You can type ${500 - x} more letters.`;
+    }
+    else if (x > 500) {
+        remainingCharsText.textContent = `Deleting ${x - 500} letters is needed.`;
+    }
+});
 
-function textareaValidation() {
-  var remaining500 = Max_Chars - textarea.value.length;
-  var remaining50 = Min_Chars - textarea.value.length;
-  let x = textarea.value.length;
-  if (x < 50) {
-    remainingCharsText.textContent = ` ${50 - x} more lettrs are needed.`;
-  } else if (50 < x && x < 500) {
-    remainingCharsText.textContent = `You can type ${500 - x} more letters.`;
-  } else if (x > 500) {
-    remainingCharsText.textContent = `Deleting ${x - 500} letters is needed.`;
-  }
+var textarea = document.getElementById('Message');
+var remainingCharsText = document.getElementById('remaining-char');
+var Max_Chars = 500;
+var Min_Chars = 50;
+
+textarea.addEventListener('input',textvalid())
+function textvalid(){
+    var remaining500 = Max_Chars - textarea.value.length;
+    var remaining50 = Min_Chars - textarea.value.length;
+    let x = textarea.value.length;
+    if (x < 50) {
+        remainingCharsText.textContent = ` ${50 - x} more lettrs are needed.`;
+        return false;
+    }
+    else if (50 < x && x < 500) {
+        remainingCharsText.textContent = `You can type ${500 - x} more letters.`;
+        return true;
+    }
+    else if (x > 500) {
+        remainingCharsText.textContent = `Deleting ${x - 500} letters is needed.`;
+        return false;
+    }
 }
 
-//contact page submit
-document.getElementById('contact-submit').addEventListener("click", submit)
-
-function submit(e) {
-  if (
-    nameValidation() &&
-    emailValidation() &&
-    phoneValidation() &&
-    textareaValidation()
-  ) {
-    alert("valid");
-  } else {
-    alert("invalid");
-  }
-}
 
 //for password validation
-function passwordValidation() {
+function passwordValidation(){
   var passwordRegExp = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,20}$/);
   var password = document.getElementById("password");
 
-  if (passwordRegExp.test(password)) {
+  if (passwordRegExp.test(password)){
     return true;
   }
   return false;
 }
 
-function conPassword() {
+function conPassword(){
   var password = document.getElementById("password");
   var confirmPassword = document.getElementById("confirmpassword");
 
-  if (password != confirmpassword) {
+  if (password != confirmpassword){
     return false;
   }
   return true;
 }
 
 //for 3 char
-function atleast3char() {
+function atleast3char(){
   var fname = document.getElementById("fname").value;
   var lname = document.getElementById("lname").value;
   var address = document.getElementById("address").value;
@@ -108,62 +299,50 @@ function atleast3char() {
 
   var atleast3charRegExp = new RegExp(/^[a-zA-Z]{3,}$/);
 
-  if (atleast3charRegExp.test(fname)) {
+  if (atleast3charRegExp.test(fname)){
     return true;
   }
-  if (atleast3charRegExp.test(lname)) {
+  if (atleast3charRegExp.test(lname)){
     return true;
   }
-  if (atleast3charRegExp.test(address)) {
+  if (atleast3charRegExp.test(address)){
     return true;
   }
-  if (atleast3charRegExp.test(city)) {
+  if (atleast3charRegExp.test(city)){
     return true;
-  } else {
+  }
+  else {
     return false;
   }
 }
 
 //for zipcode
-function zipcodevalidation() {
-  var zipcode = document.getElementById("zipcode").value;
+function zipcodevalidation(){
+  var zipcode = document.getElementById("zipcode");
   var zipRegExp = new RegExp(/^\d{4,6}$/);
 
-  if (zipRegExp.test(zipcode)) {
+  if(zipRegExp.test(zipcode)){
     return true;
   }
   return false;
 }
 
-//for profile image
-var imageAvatar = document.querySelector('#oval-img');
+//Final
+var submit = document.getElementById("contact-submit");
 
-function check_img_input(input) {
-  if (input.files.length == 0) {
-    return 0;
-  } else {
-    return 1;
+submit.addEventListener("click", function(e){
+  e.preventDefault()
+
+  if (
+    nameValidation()
+    && emailValidation()
+    && phoneValidation()
+    && radioValidation()
+    && textvalid()
+  ) {
+    alert("valid");
   }
-}
-
-function change_img(input) {
-  if (check_img_input(input)) {
-    const file = document.querySelector('input[type=file]').files[0];
-    const reader = new FileReader();
-    reader.addEventListener("load", function() {
-      // convert image file to base64 string
-      imageAvatar.src = reader.result;
-    }, false);
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
+  else {
+    alert("invalid");
   }
-}
-
-//for exposing additional fields
-var shopper = document.getElementById("store_owener");
-
-shopper.click(function radio() {
-      (".reveal-if-active").show();
-    }
+});
