8eb4aeceb2cfcea523a83ddf7b718930e2054167
Quan Do <aloha.liamdo@gmail.com>
2021-05-22 11:19:21 +0000

    Finished PHP to prepare CSV file

---
+++ PHP/CSV.php
@@ -0,0 +1,39 @@
+<?php
+
+function create_associative_array($data)
+{
+  // Pick which CSV file to run
+  switch ($data) {
+    case $data == "stores":
+      $file = fopen("../Data/stores.csv", "r");
+      break;
+    case $data == "products":
+      $file = fopen("../Data/products.csv", "r");
+      break;
+    case $data == "categories":
+      $file = fopen("../Data/categories.csv", "r");
+      break;
+    default:
+      exit("Pick between 'stores', 'products', 'categories'");
+      break;
+  }
+  $header = fgetcsv($file); // Get the header to use later
+  $result = [];
+  // While $row = fgetcsv can still be run
+  while (($row = fgetcsv($file)) !== false) {
+    // Combine the header and the row into an array, then add to result
+    $result[] = array_combine($header, $row);
+  }
+  return $result;
+}
+
+function get_item($data, $id)
+{
+  $associative_array = create_associative_array($data);
+  $item = $associative_array[$id - 1];
+  return $item;
+}
+
+echo "<pre>";
+print_r(get_item("categories", 5));
+echo "</pre>";
