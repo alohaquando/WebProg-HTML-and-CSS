2f01054058a880938b856e410930c2dec43a301d
Quan Do <aloha.liamdo@gmail.com>
2021-05-22 11:22:32 +0000

    More guide

--- PHP/CSV.php
+++ PHP/CSV.php
@@ -1,5 +1,6 @@
 <?php
 
+// Create associative array containing many rows of store/cat./products inside.
 function create_associative_array($data)
 {
   // Pick which CSV file to run
@@ -19,7 +20,7 @@ function create_associative_array($data)
   }
   $header = fgetcsv($file); // Get the header to use later
   $result = [];
-  // While $row = fgetcsv can still be run
+  // While $row = fgetcsv can still be run (isn't false)
   while (($row = fgetcsv($file)) !== false) {
     // Combine the header and the row into an array, then add to result
     $result[] = array_combine($header, $row);
@@ -27,6 +28,7 @@ function create_associative_array($data)
   return $result;
 }
 
+// Using the associative array, use the ID to get the detail
 function get_item($data, $id)
 {
   $associative_array = create_associative_array($data);
@@ -35,5 +37,5 @@ function get_item($data, $id)
 }
 
 echo "<pre>";
-print_r(get_item("categories", 5));
+print_r(get_item("stores", 12));
 echo "</pre>";
