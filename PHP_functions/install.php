8f69cd8958531fad2c63dda2bf72bbb7421d480e
hoangphamducanh <hoang.phamducanh@gmail.com>
2021-05-31 16:57:25 +0700

    installphp

---
+++ PHP_functions/install.php
@@ -0,0 +1,22 @@
+<?php 
+include('../functions.php');
+
+if (!isAdmin()) {
+	$_SESSION['msg'] = "You must log in first";
+	header('location: ../login.php');
+}
+
+if (isset($_GET['logout'])) {
+	session_destroy();
+	unset($_SESSION['user']);
+	header("location: ../login.php");
+}
+
+function isAdmin()
+{
+	if (isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'admin' ) {
+		return true;
+	}else{
+		return false;
+	}
+}
