5b55c17045c22f249d153fffa2e2d9d5f43964b5
hoangphamducanh <hoang.phamducanh@gmail.com>
2021-06-01 19:55:03 +0700

    logincmspage

---
+++ Login_CMS.php
@@ -0,0 +1,64 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>My account</title>
+    <link rel="stylesheet" href="CSS/main.css" />
+    <meta name="viewpoint" content="width=device-width, initial-scale=1" />
+    <script
+      src="https://kit.fontawesome.com/f43db195aa.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <header id="nav_header"></header>
+    <div class="body_spacing">
+      <div class="HeaderH1_Left_With_Spacing">
+        <h1>Log in</h1>
+      </div>
+      <form name="loginForm" action="5.1.7-My-account-Logged-in.html">
+
+        <!-- Login method -->
+        <div class="styled-radio">
+          <p>Log in with</p>
+          <input
+            type="radio"
+            id="login_method_email"
+            name="login_method"
+            value="login_method_email"
+            required
+            checked
+          />
+          <label for="login_phone">Phone</label>
+        </div>
+
+        <!-- Password -->
+        <div class="styled-input-text">
+          <label for="login_password">Password</label><br />
+          <input
+            type="password"
+            id="login_password"
+            name="login_password"
+            placeholder="Password"
+            pattern="password"
+            required
+          />
+        </div>
+
+        <div class="button-group-for-input">
+          <br />
+          <button input type="submit">Log in</button>
+        </div>
+      </form>
+
+    </div>
+    <footer id="mall_footer"></footer>
+    <div id="cookie-consent-message"></div>
+    <script src="JS/global-load-mall-header-and-footer.js"></script>
+    <script src="JS/global-load-store-header-and-footer.js"></script>
+    <script src="JS/global-mobile-nav.js"></script>
+    <script src="JS/global-logged-in-behavior.js"></script>
+    <script src="JS/1-cookie.js"></script>
+    <script src="JS/5-login.js"></script>
+  </body>
+</html>
