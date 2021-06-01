dcc4bca2296d0ec3b7287a5c2654341f48fcb8f3
hoangphamducanh <hoang.phamducanh@gmail.com>
2021-06-01 23:56:58 +0700

    add TODO

--- PHP_functions/admin_check.php
+++ PHP_functions/admin_check.php
@@ -7,3 +7,10 @@ if (file_exists($filename)) {
     echo "The file $filename does not exist";
 }
 ?>
+
+<!-- TODO 
+
+CHECK files exists = die
+CHECK files does not exist = other sites can run normally
+
+-->
